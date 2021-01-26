bffedc58d93d59bb29cea259b9c1c479fcc7bae5
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2021-01-26 05:09:30 -0500

    componente navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,31 +5,38 @@ const NavBar = () => {
     return (
         <>
             <header>
-                <nav className='bg-secondary d-flex justify-content-between align-items-center px-4'>
+               
+                <nav className="navbar navbar-expand-lg bg-secondary d-flex justify-content-between align-items-center px-4">
                     <div className="col col-6">
                         <img src={logo} className='scale-logo'/>
                     </div>
-                    <div className='col col-3 d-flex justify-content-center'>
-                        <button className='btn btn-primary rounded-pill mx-2'>Ingreso</button>
-                        <button className='btn btn-primary rounded-pill'>Registro</button>
-                    </div>
-                    <div className='col mx-2 text-white d-flex justify-content-end'>
-                        <i className="fas fa-cart-arrow-down"></i>
-                        <span className='badge rounded-pill bg-primary align-self-center fs-6 mx-1'>1</span>
-                    </div>
-                    <div className="col">
-                        <button className='btn btn-light text-secondary'><i className="fas fa-bars"></i></button>
-                        <ul className='bg-light p-4 text-secondary text-uppercase list-menu rounded-3 d-none'>
-                            <li><i class="fas fa-tag"></i>Categorías</li>
-                            <hr/>
-                            <li><a href="#" className='text-secondary'>Tecnología</a></li>
-                            <li><a href="#" className='text-secondary'>Hogar</a></li>
-                            <li><a href="#" className='text-secondary'>Deportes</a></li>
-                            <li><a href="#" className='text-secondary'>Belleza</a></li>
-                            <li><a href="#" className='text-secondary'>Libros</a></li>
-                            <li><a href="#" className='text-secondary'>Abarrotes</a></li>
-                            <li><i class="fas fa-heart"></i>Favoritos</li>
-                        </ul>
+                    <div className="col-6 d-flex align-items-center">
+                        <div className='col-7 d-flex justify-content-end'>
+                            <button className='btn btn-primary rounded-pill mx-2'>Ingreso</button>
+                            <button className='btn btn-primary rounded-pill'>Registro</button>
+                        </div>
+                        <div className='col text-white d-flex mx-3'>
+                            <i className="fas fa-cart-arrow-down"></i>
+                            <span className='badge rounded-pill bg-primary align-self-center fs-6 mx-1'>1</span>
+                        </div>
+                        
+                        <div className="col collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul className="navbar-nav">
+                                <li className="nav-item dropdown">
+                                    <button class="btn btn-light text-secondary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i className="fas fa-bars"></i>
+                                    </button>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a className="dropdown-item" href="#">Tecnología</a>
+                                        <a className="dropdown-item" href="#">Hogar</a>
+                                        <a className="dropdown-item" href="#">Deportes</a>
+                                        <a className="dropdown-item" href="#">Belleza</a>
+                                        <a className="dropdown-item" href="#">Libros</a>
+                                        <a className="dropdown-item" href="#">Abarrotes</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </header>
