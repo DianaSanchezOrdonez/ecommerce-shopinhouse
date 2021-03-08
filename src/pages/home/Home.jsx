13d1890aabf201a936cd105a0a15a0cd7ca3dd02
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2021-03-08 00:50:24 -0500

    se agrego footer a la vista del home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Breadcrumbs from "../../components/breadcrumbs/Breadcrumbs";
 import Footer from "../../components/footer/Footer";
-import Slider from '../../components/slider/Slider';
-import ItemSlideContainer from '../../containers/itemslidecontainer/ItemSlideContainer';
+import Slider from "../../components/slider/Slider";
+import ItemSlideContainer from "../../containers/itemslidecontainer/ItemSlideContainer";
 
 const Home = () => {
   return (
-    <section className="home">
-      {/* <Breadcrumbs/> */}
-      <Slider/>
-      <ItemSlideContainer />
-      {/* <ItemDetailContainer/>  */}
-    </section>
+    <div className="container-all d-flex-column">
+      <section className="home">
+        {/* <Breadcrumbs/> */}
+        <Slider />
+        <ItemSlideContainer />
+        {/* <ItemDetailContainer/>  */}
+      </section>
+      <Footer />
+    </div>
   );
 };
 
